c033e2f25f93ec87b47d1d91fccdf867ba3bbbb0
kapilan <km2717@imperial.ac.uk>
2019-01-25 15:14:55 +0000

    Initial Commit

---
+++ CrowdBeats/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  CrowdBeats
+//
+//  Created by Kapilan M on 25/01/2019.
+//  Copyright © 2019 kapilan. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
