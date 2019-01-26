ce8fc27bcb896c7236f3d807a99b184c38e69ea1
Lloyd <lloyd.cllwes@gmail.com>
2019-01-26 19:26:03 +0000

    Add Alamofire

---
+++ Podfile
@@ -0,0 +1,3 @@
+target "CrowdBeats" do
+	pod 'Alamofire', '~> 5.0.0.beta.1'
+end
---
+++ Pods/Target Support Files/Pods-CrowdBeats/Pods-CrowdBeats-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CrowdBeats : NSObject
+@end
+@implementation PodsDummy_Pods_CrowdBeats
+@end
